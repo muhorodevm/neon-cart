176b9a87b5c51e569ad1da93bb71f486ff9c469a
muhorodevm <fullstackdev26m@gmail.com>
2025-11-07 22:39:58 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:8080",
+    origin: "https://ndulakali.vercel.app" || "http://localhost:8080",
     credentials: true,
   })
 );
